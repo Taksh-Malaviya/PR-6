e17ce117fd3e9d4cbcfb938b53c686e737884eca
Taksh-Malaviya <150029733+Taksh-Malaviya@users.noreply.github.com>
2023-12-18 18:16:07 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,3 +1,19 @@
+/*Q.1 Write a Program to check whether a string is a palindrome or not without using string functions.
+For example,
+Input:
+Enter any string: nayan
+
+Output:
+Given string is a Palindrome.
+
+------------------------------------------------
+
+For example,
+Input:
+Enter any string: hello
+
+Output:
+Given string is not a Palindrome*/
 #include<stdio.h>
 #include<string.h>
 
